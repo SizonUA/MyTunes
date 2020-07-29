91788492134608683ef545c6acf3812799d33793
Sizon <sizon@vidrada.org>
2020-07-29 10:41:11 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,39 @@
+import {
+  musicPlayerInit
+} from "./musicPlayer.js";
+
+import {
+  radioPlayerInit
+} from "./radioPlayer.js";
+
+import {
+  videoPlayerInit
+} from "./videoPlayer.js";
+
+
+//
+const playerBtn = document.querySelectorAll(".player-btn"),
+  playerBlock = document.querySelectorAll(".player-block"),
+  temp = document.querySelector(".temp");
+
+
+// Function
+const deactivationPlayer = () => {
+  temp.style.display = "none";
+  playerBtn.forEach(item => item.classList.remove("active"));
+  playerBlock.forEach(item => item.classList.remove("active"));
+}
+
+
+// Event
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
+  deactivationPlayer();
+  btn.classList.add("active");
+  playerBlock[i].classList.add("active");
+}));
+
+
+// Call
+musicPlayerInit();
+radioPlayerInit();
+videoPlayerInit();
