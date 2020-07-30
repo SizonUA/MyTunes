e1572544f5a1a8506567286ec54bcda46ea8b3ff
Sizon <sizon@vidrada.org>
2020-07-30 21:44:45 +0300

    add radioPlayer

--- script/radioPlayer.js
+++ script/radioPlayer.js
@@ -1,3 +1,56 @@
 export const radioPlayerInit = () => {
-  console.log("Radio Init");
+  const radio = document.querySelector(".radio"),
+    radioCoverImg = document.querySelector(".radio-cover__img"),
+    radioNavigation = document.querySelector(".radio-navigation"),
+    radioHeaderBig = document.querySelector(".radio-header__big"),
+    radioStop = document.querySelector(".radio-stop"),
+    radioItem = document.querySelectorAll(".radio-item");
+
+  const audio = new Audio();
+  audio.type = "audio/aac";
+  radioStop.disabled = true;
+
+  const changeIconPlay = () => {
+    if (audio.paused) {
+      radio.classList.remove("play");
+      radioStop.classList.add("fa-play");
+      radioStop.classList.remove("fa-stop");
+    } else {
+      radio.classList.add("play");
+      radioStop.classList.add("fa-stop");
+      radioStop.classList.remove("fa-play");
+    }
+  };
+
+  const selectItem = (elem) => {
+    radioItem.forEach((item) => item.classList.remove("select"));
+    elem.classList.add("select");
+  };
+
+  radioNavigation.addEventListener("change", (event) => {
+    const target = event.target;
+    const parent = target.closest(".radio-item");
+
+    const title = parent.querySelector(".radio-name").textContent;
+    radioHeaderBig.textContent = title;
+
+    const urlImg = parent.querySelector(".radio-img").src;
+    radioCoverImg.src = urlImg;
+
+    selectItem(parent);
+    radioStop.disabled = false;
+    audio.src = target.dataset.radioStation;
+
+    audio.play();
+    changeIconPlay();
+  });
+
+  radioStop.addEventListener("click", () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    changeIconPlay();
+  });
 };
